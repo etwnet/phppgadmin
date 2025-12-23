822c32c6f7ca023fb9a6651e74e02ea0bc5df1eb
Christian Müller <post-christian@outlook.de>
2025-12-23 23:19:52 +0100

    Add SQL Dump classes

--- classes/database/Connection.php
+++ classes/database/Connection.php
@@ -8,7 +8,11 @@
 
 include_once('./classes/database/ADODB_base.php');
 
-class Connection {
+/**
+ * @deprecated please use /libraries/Database/Connector instead.
+ */
+class Connection
+{
 
 	var $conn;
 
@@ -19,7 +23,8 @@ class Connection {
 	 * Creates a new connection.  Will actually make a database connection.
 	 * @param $fetchMode int Defaults to associative.  Override for different behaviour
 	 */
-	function __construct($host, $port, $sslmode, $user, $password, $database, $fetchMode = ADODB_FETCH_ASSOC) {
+	function __construct($host, $port, $sslmode, $user, $password, $database, $fetchMode = ADODB_FETCH_ASSOC)
+	{
 		$this->conn = ADONewConnection('postgres8');
 		$this->conn->setFetchMode($fetchMode);
 
@@ -50,10 +55,12 @@ class Connection {
 	 * @return null if version is < 7.4
 	 * @return -3 Database-specific failure
 	 */
-	function getDriver(&$description) {
+	function getDriver(&$description)
+	{
 
 		$v = pg_version($this->conn->_connectionID);
-		if (isset($v['server'])) $version = $v['server'];
+		if (isset($v['server']))
+			$version = $v['server'];
 
 		// If we didn't manage to get the version without a query, query...
 		if (!isset($version)) {
@@ -67,7 +74,8 @@ class Connection {
 				$this->platform = 'MINGW';
 
 			$params = explode(' ', $field);
-			if (!isset($params[1])) return -3;
+			if (!isset($params[1]))
+				return -3;
 
 			$version = $params[1]; // eg. 8.4.4
 		}
@@ -75,53 +83,53 @@ class Connection {
 		$description = "PostgreSQL {$version}";
 
 		// Detect version and choose appropriate database driver
-		if ((int)$version >= 10) {
+		if ((int) $version >= 10) {
 			switch (substr($version, 0, 2)) {
-			default:
-				return 'Postgres13';
-			case '12':
-				return 'Postgres12';
-			case '11':
-				return 'Postgres11';
-			case '10':
-				return 'Postgres10';
+				default:
+					return 'Postgres13';
+				case '12':
+					return 'Postgres12';
+				case '11':
+					return 'Postgres11';
+				case '10':
+					return 'Postgres10';
 			}
 		} else {
 			switch (substr($version, 0, 3)) {
-			case '9.6':
-				return 'Postgres96';
-			case '9.5':
-				return 'Postgres95';
-			case '9.4':
-				return 'Postgres94';
-			case '9.3':
-				return 'Postgres93';
-			case '9.2':
-				return 'Postgres92';
-			case '9.1':
-				return 'Postgres91';
-			case '9.0':
-				return 'Postgres90';
-			case '8.4':
-				return 'Postgres84';
-			case '8.3':
-				return 'Postgres83';
-			case '8.2':
-				return 'Postgres82';
-			case '8.1':
-				return 'Postgres81';
-			case '8.0':
-			case '7.5':
-				return 'Postgres80';
-			case '7.4':
-				return 'Postgres74';
+				case '9.6':
+					return 'Postgres96';
+				case '9.5':
+					return 'Postgres95';
+				case '9.4':
+					return 'Postgres94';
+				case '9.3':
+					return 'Postgres93';
+				case '9.2':
+					return 'Postgres92';
+				case '9.1':
+					return 'Postgres91';
+				case '9.0':
+					return 'Postgres90';
+				case '8.4':
+					return 'Postgres84';
+				case '8.3':
+					return 'Postgres83';
+				case '8.2':
+					return 'Postgres82';
+				case '8.1':
+					return 'Postgres81';
+				case '8.0':
+				case '7.5':
+					return 'Postgres80';
+				case '7.4':
+					return 'Postgres74';
 			}
 		}
 
 		/* All <7.4 versions are not supported */
 		// if major version is 7 or less and wasn't cought in the
 		// switch/case block, we have an unsupported version.
-		if ((int)substr($version, 0, 1) < 8)
+		if ((int) substr($version, 0, 1) < 8)
 			return null;
 
 		// If unknown version, then default to latest driver
@@ -133,7 +141,8 @@ class Connection {
 	 * Get the last error in the connection
 	 * @return string Error string
 	 */
-	function getLastError() {
+	function getLastError()
+	{
 		return pg_last_error($this->conn->_connectionID);
 	}
 }
